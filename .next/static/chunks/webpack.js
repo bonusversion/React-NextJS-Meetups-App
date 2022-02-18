c30a3435a5cedce50cd941c1df72022c6301b7ca
mybonus <506832550@qq.com>
2022-02-18 17:56:30 +0800

    Load all meetups data from mongoDB

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "071092d7f745fe7cae47";
+/******/ 	var hotCurrentHash = "4dfa56ac4783dca7b139";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { MongoClient } from "mongodb";
 
 const DUMMY_MEETUPS = [
   {
@@ -21,27 +22,44 @@ const HomePage = (props) => {
   return <MeetupList meetups={props.meetups} />;
 };
 
-export const getServerSideProps = async (context) => {
-  const req = context.req;
-  const res = context.res;
+// export const getServerSideProps = async (context) => {
+//   const req = context.req;
+//   const res = context.res;
 
-  // fetching data from an API
+//   // fetching data from an API
 
-  return {
-    props: {
-      meetups: DUMMY_MEETUPS,
-    },
-  };
-};
-
-// export const getStaticProps = async () => {
-//   // fetch data from an API
 //   return {
 //     props: {
 //       meetups: DUMMY_MEETUPS,
 //     },
-//     revalidate: 1,
 //   };
 // };
+
+export const getStaticProps = async () => {
+  // fetch data from an API
+  const client = await MongoClient.connect(
+    "mongodb://bonusversion:gaor5WG2A7NcPj0K@cluster0-shard-00-00.gkbnc.mongodb.net:27017,cluster0-shard-00-01.gkbnc.mongodb.net:27017,cluster0-shard-00-02.gkbnc.mongodb.net:27017/meetups?ssl=true&replicaSet=atlas-5qtmtj-shard-0&authSource=admin&retryWrites=true&w=majority"
+  );
+
+  const db = await client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+};
 
 export default HomePage;
